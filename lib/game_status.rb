5ad4661f3029384e1a36560ed707fabc4d776987
Steve Albonico <salbonico@gmail.com>
2019-01-12 02:03:07 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -10,9 +10,9 @@ WIN_COMBINATIONS =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,
 
 def won?(board)
   WIN_COMBINATIONS.each |combo| do
-   temp = combo.all? do |number|
-     position_taken(board, number)
-     end
+
+   temp = true
+
    if temp == true
      return combo
 

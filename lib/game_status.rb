7e239fad57e5b7369cc9693abd21835da4736fe2
Steve Albonico <salbonico@gmail.com>
2019-01-12 01:40:52 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,12 +6,18 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
+
+def match?(combo)
+  combo.all? do |number|
+    position_taken?(board, number)
+  end
+end
+
 def won?(board)
-  WIN_COMBINATIONS.each |combo|
-   trigger = combo.all? do |number|
-     board[number] != " "
-   if trigger == true
-     return combo
-   end
-   end
+
+  WIN_COMBINATIONS.each |combo| do
+      if match? == true
+        return combo
+  end
+  return nil
 end
